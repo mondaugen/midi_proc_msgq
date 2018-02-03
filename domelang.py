07cd635ebbdb6bdcd8ebdd3cc14319fd7b85f754
Nichoas Esterer <nicholas.esterer@gmail.com>
2018-02-02 19:40:25 -0500

    Added the possibility for vectorization
    The old tests pass but vectorization hasn't actually been tried yet.
    
    Also added a way to pass the parser state to the aux and the stack operations so
    that their behaviour can be modified accordingly.

--- domelang.py
+++ domelang.py
@@ -11,59 +11,85 @@ DOME_MODE_INTERPRET=3
 
 DEBUG=False
 
-def _plus(s,a):
-    x=s.pop()
-    y=s.pop()
-    s.append(y+x)
+def _op_plus(x,y):
+    return x+y
+def _op_times(x,y):
+    return x*y
+def _op_divide(x,y):
+    return x/y
+def _op_subtract(x,y):
+    return x-y
 
-def _times(s,a):
-    x=s.pop()
-    y=s.pop()
-    s.append(y*x)
+def _vop(x,y,op):
+    if type(x) == list:
+        lx = len(x)
+        if type(y) == list:
+            ly = len(y)
+            for i in range(lx):
+                x[i] = _vop(x[i],y[i%ly])
+        else:
+            for i in range(lx):
+                x[i] = _vop(x[i],y)
+        return x
+    else:
+        if type(y) == list:
+            return op(x,y[0])
+        else:
+            return op(x,y)
 
-def _divide(s,a):
+def _bin_op(s,a,state=None):
+    op=a
     x=s.pop()
     y=s.pop()
-    s.append(y/x)
-
-def _subtract(s,a):
-    x=s.pop()
-    y=s.pop()
-    s.append(y-x)
+    s.append(_vop(y,x,op))
+    # TODO: opt can be set to change how _bin_op works
 
 # Note these are stored in the order that they are matched against. The first
 # one to match is executed.
 cmd_parsers=[
         # Command name, pattern matcher, auxilary preparation function, function
+        # command name can be any string
+        # pattern matcher is a regex that can be interpreted by the re module to
+        # give a matching object
+        # auxilary preparation function accepts the string parsed and a state
+        # variable and returns data necessary for "function" to evalutate
+        # function is passed the stack and the auxilary function and returns its
+        # result by operating on the stack
         (
             'FLOAT',
             '[-+]?\d+\.\d*([eE][-+]?\d+|)',
-            lambda x: float(x),
-            lambda s,a: s.append(a)
+            lambda x,s: float(x),
+            lambda s,a,t: s.append(a)
         ),
         (
             'INT',
             '[-+]?\d+',
-            lambda x: int(x),
-            lambda s,a: s.append(a)
+            lambda x,s: int(x),
+            lambda s,a,t: s.append(a)
         ),
         (
             'PLUS',
             '[+]',
-            None,
-            _plus
+            lambda x,s: _op_plus,
+            _bin_op
         ),
         (
             'TIMES',
             '×',
-            None,
-            _times
+            lambda x,s: _op_times,
+            _bin_op
         ),
         (
             'DIVIDE',
             '÷',
-            None,
-            _divide
+            lambda x,s: _op_divide,
+            _bin_op
+        ),
+        (
+            'SUBTRACT',
+            '-',
+            lambda x,s: _op_subtract,
+            _bin_op
         ),
         (
             'NOP',
@@ -75,7 +101,7 @@ cmd_parsers=[
             'PRINT',
             'p',
             None,
-            lambda s,a: print(s[-1])
+            lambda s,a,t: print(s[-1])
         )
 ]
 
@@ -88,6 +114,9 @@ class Dome:
             self.cmd_parsers.append((n,re.compile(p),a,f))
         for t in cmd_parsers:
             print(t)
+        # Eventually we will use this to pass information to operators
+        self.state=None
+        
     def parse(self,stack,cmds):
         """
         Parse the commands, effecting them on the stack.
@@ -99,13 +128,13 @@ class Dome:
                 if m:
                     aux=None
                     if a:
-                        aux=a(m.group(0))
+                        aux=a(m.group(0),self.state)
                     if (DEBUG):
                         print('command: %s' % (n,))
                         print('stack: ',end='')
                         print(stack)
                     if f:
-                        f(stack,aux)
+                        f(stack,aux,self.state)
                     else:
                         if (DEBUG):
                             print("Warning, no function")
